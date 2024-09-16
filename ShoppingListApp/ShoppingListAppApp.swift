09e5f2b41d705ff6cea64f204e5d1699154b9a2b
Anlprnc <anilpirincciii@gmail.com>
2024-09-16 22:41:41 +0300

    Initial Commit

---
+++ ShoppingListApp/ShoppingListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListAppApp.swift
+//  ShoppingListApp
+//
+//  Created by Anıl on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
